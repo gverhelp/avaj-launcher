4a207ea0285796357019885aafc956a8140772b8
Garreth Verhelpen <garreth@Garreths-MacBook-Pro.local>
2023-04-25 18:42:52 +0200

    parsing done I guess

--- srcs/Simulator.java
+++ srcs/Simulator.java
@@ -18,14 +18,25 @@ public class Simulator {
 
     public static void parseFile(File file) {
         try (Scanner scanner = new Scanner(file)) {
+
+            int countLines = 0;
+
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 String[] tokens = line.split(" ");
 
-                if (tokens[0].equals("Baloon") || tokens[0].equals("JetPlane") || tokens[0].equals("Helicopter")) {
+                if (countLines == 0) {
+                    countLines++;
+                    if (tokens.length != 1 || line.length() == 0) {
+                        throw new Exception("Number of simulation error."); 
+                    }
+                    int runs = Integer.parseInt(tokens[0]);
+                }
+                else if (tokens[0].equals("Baloon") || tokens[0].equals("JetPlane") || tokens[0].equals("Helicopter")) {
                     int longitude = Integer.parseInt(tokens[2]);
                     int latitude = Integer.parseInt(tokens[3]);
                     int height = Integer.parseInt(tokens[4]);
+                    countLines++;
 
                     if (longitude < 0 || latitude < 0 || height < 0)
                         throw new Exception("Coordinates error.");
@@ -34,13 +45,15 @@ public class Simulator {
 
                     Coordinates coordinates = new Coordinates(longitude, latitude, height);
                 }
-                
+                else {
+                    throw new Exception("Type error.");
+                }
             }
             scanner.close();
         } catch (FileNotFoundException e) {
             System.out.println("File name error.");
         } catch (Exception e) {
-            e.getMessage();
+            System.out.print(e.getMessage());
         }
     }
 }
