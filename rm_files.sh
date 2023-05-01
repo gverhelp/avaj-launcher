92435f07c299e6320853b65d6fe9814d3625940e
Garreth Verhelpen <garreth@Garreths-MacBook-Pro.local>
2023-05-01 16:48:02 +0200

    zZz

--- rm_files.sh
+++ rm_files.sh
@@ -8,6 +8,10 @@ for file in $(find ./ro/academyplus/avaj/simulator -name "*.class"); do
     echo -e "${RED}Deleting $file${NC}" && rm -rf ${file}
 done
 
+if [ ! -e ./simulation.txt ] || [ ! -e ./sources.txt ]; then
+    exit 0
+fi
+
 echo -e "${RED}Deleting ./sources.txt${NC}" && rm -rf ./sources.txt
 echo -e "${RED}Deleting ./simulation.txt${NC}" && rm -rf ./simulation.txt
 echo -e "${GREEN}-> SUCCESS : Files has been deleted! <-${NC}"
